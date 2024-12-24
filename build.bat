d6f4fb6f8b578d626a18dc0d0be664ddd26c3b55
Doronin Stanislav <mugisbrows@gmail.com>
2024-12-24 22:42:07 +0300

    Qt 6.8.1

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 rem This file is generated from build.pbat, all edits will be lost
-set PATH=C:\Program Files\CMake\bin;%LOCALAPPDATA%\Programs\Python\Python313;%LOCALAPPDATA%\Programs\Python\Python313\Scripts;C:\Python313;C:\Python313\Scripts;C:\mingw1120_64\bin;C:\Qt\6.7.3\mingw1120_64\bin;%PATH%
+set PATH=C:\Program Files\CMake\bin;%LOCALAPPDATA%\Programs\Python\Python313;%LOCALAPPDATA%\Programs\Python\Python313\Scripts;C:\Python313;C:\Python313\Scripts;C:\mingw1310_64\bin;C:\Qt\6.8.1\mingw_64\bin;%PATH%
+where ninja > NUL 2>&1 || pip install ninja
 set BUILD_TYPE=Release
 if /I "%1"=="debug" set BUILD_TYPE=Debug
 echo BUILD_TYPE %BUILD_TYPE%
--- env.bat
+++ env.bat
@@ -1,3 +1,3 @@
 @echo off
 rem This file is generated from env.pbat, all edits will be lost
-set PATH=C:\Program Files\CMake\bin;%LOCALAPPDATA%\Programs\Python\Python313;%LOCALAPPDATA%\Programs\Python\Python313\Scripts;C:\Python313;C:\Python313\Scripts;C:\mingw1120_64\bin;C:\Qt\6.7.3\mingw1120_64\bin;%PATH%
+set PATH=C:\Program Files\CMake\bin;%LOCALAPPDATA%\Programs\Python\Python313;%LOCALAPPDATA%\Programs\Python\Python313\Scripts;C:\Python313;C:\Python313\Scripts;C:\mingw1310_64\bin;C:\Qt\6.8.1\mingw_64\bin;%PATH%
